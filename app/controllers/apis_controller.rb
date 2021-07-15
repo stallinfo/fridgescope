9e95c5b5da4dd7178198ec5b164a05824884adba
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-07-15 15:12:38 +0900

    upload image 02

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -45,8 +45,9 @@ class ApisController < ApplicationController
         password = params[:password]
         facility_manager = FacilityManager.find_by(identify: identify)
         if facility_manager.authenticate(password)
+            facility = Facility.find(facility_manager.facility_id)
             initial_storage_rate = density_calculation(image)
-            Fridge.create(name: "test upload", initial_picture_path: image, description: title, facility_id: 1, latitude: 34.96044797500092, longitude: 138.4044472577484, update_count: 0, initial_storage_rate: initial_storage_rate)
+            Fridge.create(name: "test upload", initial_picture_path: image, description: title, facility_id: facility.id, latitude: 34.96044797500092, longitude: 138.4044472577484, update_count: 0, initial_storage_rate: initial_storage_rate)
         end
         render json: {result: 0}
     end
