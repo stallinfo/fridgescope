0f7b7dd9713d9519925c0343acc56f7abf5f36c7
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-07-20 15:52:45 +0900

    fix again 18

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -59,7 +59,7 @@ class ApisController < ApplicationController
                 if fridge.fridge_latest_states.count > 0
                     fridge_last = fridge.fridge_latest_states.first
                     update_count = fridge_last.update_count + 1
-                    fridge_last.update_attributes(current_picture_path: image, current_storage_rate: initial_storage_rate, update_count: update_count, updated_by: facility_manager.identify)
+                    fridge_last.update(current_picture_path: image, current_storage_rate: initial_storage_rate, update_count: update_count, updated_by: facility_manager.identify)
                 else
                     FridgeLatestState.create(current_picture_path: image, fridge_id: fridge_id, update_count: 0, created_by: facility_manager.identify, current_storage_rate: initial_storage_rate)
                 end
