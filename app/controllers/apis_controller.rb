f7b6f254595272e4cf314984f551df9c040b3d6b
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-07-20 11:33:45 +0900

    rate to_i

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -79,11 +79,7 @@ class ApisController < ApplicationController
                     fridges[fridge_count]["date"] = fridge.updated_at
                     fridges[fridge_count]["rate"] = fridge.initial_storage_rate
                     if fridge.initial_storage_rate != nil && facs[facs_count]["rate"] > fridge.initial_storage_rate
-                        facs[facs_count]["rate"] = fridge.initial_storage_rate
-                        
-                    else
-                        facs[facs_count]["rate"] = 0
-       
+                        facs[facs_count]["rate"] = fridge.initial_storage_rate.to_i
                     end
                     if fridge.initial_picture_path.attached?
                         fridges[fridge_count]["image"] = rails_blob_path(fridge.initial_picture_path , only_path: true)
