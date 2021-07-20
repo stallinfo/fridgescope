b2eb0e6660d1a3254d34cbaa80ae7ac5ec42837a
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-07-20 14:06:11 +0900

    gambling 06

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -58,11 +58,11 @@ class ApisController < ApplicationController
                 if fridge.fridge_latest_states.count > 0
                     fridge_last = fridge.fridge_latest_states.first
                     update_count = fridge_last.update_count + 1
-                    fridge_last.update_attributes(name: title, current_picture_path: image, current_storage_rate: initial_storage_rate, update_count: update_count, updated_by: facility_manager.identify)
+                    fridge_last.update_attributes(current_picture_path: image, current_storage_rate: initial_storage_rate, update_count: update_count, updated_by: facility_manager.identify)
                 else
-                    FridgeLatestState.create(name: title, current_picture_path: image, description: "", fridge_id: fridge_id, update_count: 0, created_by: facility_manager.identify, current_storage_rate: initial_storage_rate)
+                    FridgeLatestState.create(current_picture_path: image, description: "", fridge_id: fridge_id, update_count: 0, created_by: facility_manager.identify, current_storage_rate: initial_storage_rate)
                 end
-                FridgePastState.create(name: title, current_picture_path: image, description: "", fridge_id: fridge_id, update_count: 0, created_by: facility_manager.identify, current_storage_rate: initial_storage_rate)
+                FridgePastState.create(current_picture_path: image, description: "", fridge_id: fridge_id, update_count: 0, created_by: facility_manager.identify, current_storage_rate: initial_storage_rate)
             end
         end
         render json: {result: 0}
